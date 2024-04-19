451b33c05a656552474286714cb1c88ce0db0d6d
Juliana Tyelle <jujutyelle@gmail.com>
2024-04-19 10:59:04 -0300

    crud

---
+++ app_estoque/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const estoque = require('./src/estoque');
+const app     = express();
+
+app.get('/adicionar/:id/:nome/:qtd', function (req, res){ //cadastro de produto
+    let id   = req.params.id;
+    let nome = req.params.nome;
+    let qtd  = req.params.qtd;
+    let p    = estoque.criarProduto(id, nome, qtd);
+    estoque.adicionarProduto(p);
+    res.send(p);
+});
+
+app.get('/listar', function (req, res){
+    res.send(estoque.listarProdutos());
+});
+
+app.get('/remover/:id', function (req, res){
+    let id = req.params.id;
+    estoque.removerProduto(id);
+    res.send(id);
+});
+
+const PORT = 8080;
+app.listen(PORT, function(){
+    console.log('app rodando na porta ' + PORT);
+});
+
+//fazer uma função update
+
+//essa aplicação não é considerada CRUD pois esses dados não persistem, eles são carregados em memória. Ou seja, não fica salvo, toda vez é necessário reescrever.
---
+++ app_estoque/src/estoque.js
@@ -0,0 +1,34 @@
+let produtos = []; //array de controle interno da aplicacao, não será visível
+
+function criarProduto(id, nome, qtd){
+    let p = {
+        id: id,
+        nome: nome,
+        qtd: qtd
+    };
+
+    return p;
+
+}
+
+function adicionarProduto(p){
+    produtos.push(p);
+}
+
+function listarProdutos(){
+    return produtos;
+}
+
+function removerProduto(id){ 
+    produtos = produtos.filter((p) => { //funcao estilo arrow. Essa função retorna uma cópia e sobrescreve a minha lista de produtos
+        return p.id != id; //percorrer a lista
+    });
+}
+
+
+module.exports = {
+    criarProduto,
+    adicionarProduto,
+    listarProdutos,
+    removerProduto
+};
